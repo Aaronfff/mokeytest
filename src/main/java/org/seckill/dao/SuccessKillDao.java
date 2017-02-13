a2a519f66128c8c34f546c619a496aa386b44f8a
unknown <>
2017-02-13 22:38:24 +0800

    Initial commit

---
+++ src/main/java/org/seckill/dao/SuccessKillDao.java
@@ -0,0 +1,25 @@
+package org.seckill.dao;
+
+import org.seckill.entity.SuccessKilled;
+
+/**
+ * Created by ww on 2017/2/12.
+ */
+public interface SuccessKillDao {
+
+    /**
+     * 插入购买明细,可过滤重复
+     * @param seckillId
+     * @param userPhone
+     * @return 插入的结果集数量，插入的行数
+     */
+    int insertSuccessKilled(long seckillId, long userPhone);
+
+    /**
+     * 根据id查询SuccessKillId并携带秒杀产品对象实体
+     * @param seckillId
+     * @return
+     */
+    SuccessKilled queryByIdWithseckill(long seckillId);
+
+}
